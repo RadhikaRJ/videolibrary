08a46553df357a11e24957d24b2997c7e6f59c0e
RadhikaRJ <radhika7002@gmail.com>
2021-05-10 16:30:21 +0530

    refactor: Replace Link with NavLink

--- src/displaycomponents/navigation/navbar.js
+++ src/displaycomponents/navigation/navbar.js
@@ -1,9 +1,10 @@
 import "../../styles/nav.css";
 import "../../styles/button.css";
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 import {useAuth} from "../../contexts/authcontext";
  function Nav(){
-
+     
+    
     const {authState}=useAuth();
 
     return(<div>
@@ -11,19 +12,19 @@ import {useAuth} from "../../contexts/authcontext";
                             <h1 class="nav-brand">🌼 Baghbaani 🌼</h1>
                             <ul class="list-non-bullet nav-pills">
                                 <li class="list-item-inline">
-                                    <Link to="/" class="link" ><span class="material-icons md-24">home</span></Link>
+                                    <NavLink activeClassName="activeNav" to="/" class="link" end><span class="material-icons md-24">home</span></NavLink>
                                 </li>
                                 <li class="list-item-inline">
-                                    <Link to="/watchLaterList" class="link"><span class="material-icons md-24">watch_later</span></Link>
+                                    <NavLink activeClassName="activeNav" to="/watchLaterList" class="link"><span class="material-icons md-24">watch_later</span></NavLink>
                                 </li>
                                 <li class="list-item-inline">
-                                    <Link to="/private/Playlist" class="link"  ><span class="material-icons md-24">playlist_add</span></Link>
+                                    <NavLink activeClassName="activeNav" to="/private/Playlist" class="link"  ><span class="material-icons md-24">playlist_add</span></NavLink>
                                 </li>
                                 <li class="list-item-inline">
-                                    <Link to="/favorites" class="link"><span class="material-icons">favorite</span></Link>
+                                    <NavLink activeClassName="activeNav" to="/favorites" class="link"><span class="material-icons">favorite</span></NavLink>
                                 </li>
                                 <li class="list-item-inline">
-                                    <Link to="/login" class="link"><span class="material-icons-two-tone">{authState.isUserLogin ? "logout": "login"}</span></Link>
+                                    <NavLink activeClassName="activeNav" to="/login" class="link"><span class="material-icons-two-tone">{authState.isUserLogin ? "logout": "login"}</span></NavLink>
                                 </li>
                             </ul>
                         </nav>
